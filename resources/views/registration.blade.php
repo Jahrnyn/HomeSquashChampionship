7d499ec7d33e41c12c847b7fa6c601acab6133f3
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-06-26 13:21:09 +0200

    working with registration form and validation

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -4,22 +4,35 @@
         <form action="register" method="POST" id="registration-form">
 
             @csrf 
-            
+
             <div class="form-group">
                 <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
+                <input value="{{old('username')}}" id="username" name="username" required>
+                @error('username')
+                <p>{{$message}}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
+                <input value="{{old('email')}}" type="email" id="email" name="email" required>
+                @error('email')
+                <p>{{$message}}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required>
+                @error('password')
+                <p>{{$message}}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" required>
+                <input type="password" id="confirm-password" name="password_confirmation" required>
+
+                @error('password_confirmation')
+                <p>{{$message}}</p>
+                @enderror
             </div>
             <button type="submit">Register</button>
         </form>
