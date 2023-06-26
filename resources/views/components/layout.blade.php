5d2abf8775b65ad655a5de13b40df9337b7fe9a4
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-06-26 10:35:36 +0200

    created a layout for header, footer, and countdown

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squash Results</title>
+    <link rel="stylesheet" href="/main.css">
+</head>
+
+<body>
+    {{-- Header --}}
+    <header class="header">
+        <div class="logo">
+            <h1><a href="/">Squash Results</a></h1>
+        </div>
+        <div class="login-form">
+            <form>
+                <input type="email" name="email" placeholder="Email Address">
+                <input type="password" name="password" placeholder="Password">
+                <button type="submit">Login</button>
+            </form>
+            <div class="register-link">
+                <form action="/register">
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        </div>
+    </header>
+    {{-- Countdown --}}
+    <section class="countdown">
+        <h2>
+            <p id="demo"></p>
+        </h2>
+    </section>
+
+    {{-- There goes the unice content linked to the rout --}}
+    {{$slot}}
+
+    {{-- footer --}}
+    <footer>
+        <p>&copy; 2023 Squash Results. All rights reserved.</p>
+    </footer>
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Squash Results</title>
-    <link rel="stylesheet" href="/main.css">
-</head>
-
-<body>
-    {{-- Header --}}
-    <header class="header">
-        <div class="logo">
-            <h1><a href="/">Squash Results</a></h1>
-        </div>
-        <div class="login-form">
-            <form>
-                <input type="email" name="email" placeholder="Email Address">
-                <input type="password" name="password" placeholder="Password">
-                <button type="submit">Login</button>
-            </form>
-            <div class="register-link">
-                <form action="/register">
-                    <button type="submit">Register</button>
-                </form>
-            </div>
-        </div>
-    </header>
-    {{-- Countdown --}}
-    <section class="countdown">
-        <h2>
-            <p id="demo"></p>
-        </h2>
-    </section>
-    {{-- Registration --}}
+<x-layout>
     <div class="registration-container">
         <h2>Registration</h2>
         <form>
@@ -58,5 +23,5 @@
     </div>
     <script src="/main.js"></script>
 </body>
-
 </html>
+</x-layout>
