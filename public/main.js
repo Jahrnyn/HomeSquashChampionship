8e89dc2cd828a2ea71de921d26a7727ecc69fa8b
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-07-04 15:50:04 +0200

    modified the slidehsow to change automatically

--- public/main.js
+++ public/main.js
@@ -55,33 +55,39 @@ if (toastContainer) {
 
 
 
-
 /*  Slideshow */
 let slideIndex = 1;
 showSlides(slideIndex);
 
+let slideTimer = setInterval(() => {
+  plusSlides(1);
+}, 5000);
+
 // Next/previous controls
 function plusSlides(n) {
+  clearInterval(slideTimer);
+  slideTimer = setInterval(() => {
+    plusSlides(1);
+  }, 3000);
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+  clearInterval(slideTimer);
+  slideTimer = setInterval(() => {
+    plusSlides(1);
+  }, 5000);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
 }
