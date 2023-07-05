8c18a7273f8598d3bbe2e4dabacfcc38e29e2b6c
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-07-05 10:30:48 +0200

    outsorced the slideshow js

--- public/main.js
+++ public/main.js
@@ -55,39 +55,3 @@ if (toastContainer) {
 
 
 
-/*  Slideshow */
-let slideIndex = 1;
-showSlides(slideIndex);
-
-let slideTimer = setInterval(() => {
-  plusSlides(1);
-}, 5000);
-
-// Next/previous controls
-function plusSlides(n) {
-  clearInterval(slideTimer);
-  slideTimer = setInterval(() => {
-    plusSlides(1);
-  }, 5000);
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  clearInterval(slideTimer);
-  slideTimer = setInterval(() => {
-    plusSlides(1);
-  }, 5000);
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
--- resources/views/components/slideshow.blade.php
+++ resources/views/components/slideshow.blade.php
@@ -31,3 +31,4 @@
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>            
 </div>
+<script src="js/slideshow.js"></script>
